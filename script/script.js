b041e56c903fac18d0742abbf3ef8540690b64e7
RyanPanini7 <ryanmpanini@gmail.com>
2024-10-23 22:15:27 -0300

    novas mudancas

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-console.log('teste')
+class Todo{
+    constructor(){
+        this.totalTasks = document.querySelectorAll('.task').length;
+    }
+    addTask() {
+        console.log("funcionou")
+    }
+
+}
+
+let todo = new Todo();
+
+// Eventos
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    let taskText = document.querySelector('#task');
+    console.log(taskText.value);
+});
