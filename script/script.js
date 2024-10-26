66f26279aada6d95a26fa76aedd6dc30d63546d7
RyanPanini7 <ryanmpanini@gmail.com>
2024-10-26 16:43:22 -0300

    Atualizacoes

--- script/script.js
+++ script/script.js
@@ -2,19 +2,37 @@ class Todo{
     constructor(){
         this.totalTasks = document.querySelectorAll('.task').length;
     }
-    addTask() {
-        console.log("funcionou")
+    // implementando a lista com tarefas
+    addTask(taskText) {
+        
+        //clonar o tamplate
+        let template = document.querySelector('.task').cloneNode(true);
+        //remover classe hide
+        template.classList.remove('hide');
+        //Manipular texto
+        let templateText = template.querySelector('.task-title');
+        templateText.textContent = taskText;
+        //pegar lista e inserir nela
+        let list = document.querySelector('#tasks-container')
+        list.appendChild(template)
     }
-
+ 
 }
 
 let todo = new Todo();
 
 // Eventos
-const addBtn = document.getElementById('add');
+let addBtn = document.querySelector('#add');
 
 addBtn.addEventListener('click', function(e){
     e.preventDefault();
     let taskText = document.querySelector('#task');
-    console.log(taskText.value);
+    // nao chamar o method se estiver vazio, apagar empty.
+    if(taskText.value != ''){
+        todo.addTask(taskText.value)
+        let empty = document.querySelector()
+    }
+
+    //limpa campo de texto 
+    taskText.value = '';
 });
