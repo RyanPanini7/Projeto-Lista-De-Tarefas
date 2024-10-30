9239248e1c394e03737cacc2408c0e39913cabf9
RyanPanini7 <ryanmpanini@gmail.com>
2024-10-29 22:39:52 -0300

    adicionado eventos

--- script/script.js
+++ script/script.js
@@ -13,10 +13,34 @@ class Todo{
         let templateText = template.querySelector('.task-title');
         templateText.textContent = taskText;
         //pegar lista e inserir nela
-        let list = document.querySelector('#tasks-container')
-        list.appendChild(template)
+        let list = document.querySelector('#tasks-container');
+        list.appendChild(template);
+
+        // Adiciona evento as tasks
+        this.addEvents();
+    }
+    removeTask(task){
+        console.log('Deu certo!')
+    }
+    completeTask(task){
+        console.log('Top!')
     }
  
+    addEvents() {
+        let removeBtns = document.querySelectorAll('.fa-trash');
+        let removeBtn = removeBtns[removeBtns.length - 1];
+        let doneBtns = document.querySelectorAll('.fa-check');
+        let doneBtn = doneBtns[doneBtns.length - 1];
+        // adicionar evento de remover
+        removeBtn.addEventListener('click', function(){
+            todo.removeTask(this);
+        });
+
+    //Adicionar evento de completar tarefa
+        doneBtn.addEventListener('click', function(){
+            todo.completeTask(this);
+        });
+    }
 }
 
 let todo = new Todo();
@@ -29,8 +53,7 @@ addBtn.addEventListener('click', function(e){
     let taskText = document.querySelector('#task');
     // nao chamar o method se estiver vazio, apagar empty.
     if(taskText.value != ''){
-        todo.addTask(taskText.value)
-        let empty = document.querySelector()
+        todo.addTask(taskText.value);
     }
 
     //limpa campo de texto 
